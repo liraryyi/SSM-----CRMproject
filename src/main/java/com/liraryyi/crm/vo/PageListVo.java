259d450f08c985048dc3b04f8c5971b6a9aa272b
liraryyi <592965090@qq.com>
2021-06-20 20:12:59 +0800

    做了分页查询

---
+++ src/main/java/com/liraryyi/crm/vo/PageListVo.java
@@ -0,0 +1,12 @@
+package com.liraryyi.crm.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter @Getter
+public class PageListVo<T> {
+    private int total;
+    private List<T> list;
+}
---
+++ src/main/java/com/liraryyi/crm/workbench/dao/Activity_remark.java
@@ -0,0 +1,4 @@
+package com.liraryyi.crm.workbench.dao;
+
+public interface Activity_remark {
+}
